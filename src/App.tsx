043b62a22cbaf56dffc832d12e7d3bd4acaae5b7
grey-gp <grey.grahamp@gmail.com>
2023-08-30 15:23:35 -0400

    Starting to create directory app using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type nameState = {
+    name: string
+}
+
+class App extends Component<{}, nameState> {
+
+    state: nameState = {
+        name: 'Graham'
+    };
+
+    render() {
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Hi {this.state.name}</p>
+                    <button onClick={
+                        () => {
+                            this.setState((state, props) => {
+                                return {
+                                    name: 'Anessa'
+                                };
+                            }, () => {})
+                        }
+                    }>Click Me</button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
